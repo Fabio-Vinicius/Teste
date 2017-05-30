77a3dcf4f98800542d391a048ea66f63e3f5b264
MC1\fcosta <fabio.costa@mc1.com.br>
2017-05-30 17:29:04 -0300

    Second Try

---
+++ Grades.Tests/Types/ReferenceTypesTests.cs
@@ -0,0 +1,36 @@
+﻿using Grade;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+
+    public class ReferenceTypesTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+
+        [TestMethod]
+
+        public void GradeBookVariablesHoldReference()
+        {
+
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1 = new GradeBook();
+            g1.Name = "Scots Book";
+
+            Assert.AreNotEqual(g1.Name, g2.Name);
+
+        }
+    }
+}
