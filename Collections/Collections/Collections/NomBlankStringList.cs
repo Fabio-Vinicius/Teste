b3d94e5ba3eeca930f4da58cb27e19a2e034b2d5
MC1\fcosta <fabio.costa@mc1.com.br>
2017-06-21 18:27:47 -0300

    Commit inicial Collections

---
+++ Collections/Collections/Collections/NomBlankStringList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class NomBlankStringList: Collection<string>
+    {
+        protected override void InsertItem(int index, string item)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+            {
+                throw new ArgumentException("Elementos não podem ser nulos nem em branco");
+            }
+            else
+            {
+                base.InsertItem(index, item);
+            }
+        }
+
+        protected override void SetItem(int index, string item)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+            {
+                throw new ArgumentException("Elementos não podem ser nulos nem em branco");
+            }
+            else
+            {
+                base.InsertItem(index, item);
+            }
+        }
+
+
+    }
+}
---
+++ Collections/Collections/Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NomBlankStringList lst = new NomBlankStringList();
+
+            lst.Add("Add at index 0");
+            lst[0] = "Item changed at 0";
+
+            lst.Add("Add at index 1");
+            lst.Insert(2, "Item changed at 2");
+
+            foreach (string item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+           
+    }
+}
