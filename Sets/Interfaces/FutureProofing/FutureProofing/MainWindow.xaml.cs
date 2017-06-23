f4b2f888bd986b4aaa4cd7aee1f714fd924ed422
MC1\fcosta <fabio.costa@mc1.com.br>
2017-06-23 18:16:14 -0300

    Commit Interfaces

---
+++ Sets/Interfaces/FutureProofing/FutureProofing/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using PersonLibrary;
+
+
+namespace FutureProofing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PeopleRepository peopleRepo = new PeopleRepository();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConcreteFetchButton(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            Person[] people;
+            people = peopleRepo.GetPeople();
+
+            foreach (var person in people)
+                PersonList.Items.Add(person);
+        }
+
+        private void InterfaceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+        }
+    }
+}
---
+++ Sets/Interfaces/FutureProofing/PersonLibrary/PeopleRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class PeopleRepository
+    {
+        public Person[] GetPeople()
+        {
+            var people = new Person[]
+            {
+                new Person() { FirstName = "John", LastName = "King", Rating = 7, StartDate = DateTime.Parse("01/26/1994") },
+                new Person() { FirstName = "Mike", LastName = "Dylan", Rating = 7, StartDate = DateTime.Parse("01/26/1994") },
+                new Person() { FirstName = "Peter", LastName = "May", Rating = 7, StartDate = DateTime.Parse("01/26/1994") }
+            };
+
+            return people;
+        }
+        //public PersonLibrary GetPerson(string lastName)
+        //{
+
+        //}
+        //public void AddPerson(PersonLibrary newPerson)
+        //{
+        //}
+        //public void UpdatePerson(string lastName, PersonLibrary updatePerson)
+        //{
+
+        //}
+        //public void DeletePerson (string lastName)
+        //{
+
+        //}
+        //public void Updatepeople(List<PersonLibrary> updatePeople)
+        //{
+
+        //}
+    }
+}
